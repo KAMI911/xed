4a5a72faaedced8225a5b3e5414441a21de56112
Michael Webster <miketwebster@gmail.com>
2026-08-21 00:04:13 -0400

    xed-window.c: Chain up window_state_event.
    We can't return FALSE (GDK_EVENT_PROPAGATE) because this is a
    vfunc, not a signal handler - we must chain up.
    
    This fixes xed not 'appearing' maximized in Wayland sessions.

--- xed/xed-window.c
+++ xed/xed-window.c
@@ -277,7 +277,7 @@ xed_window_window_state_event (GtkWidget           *widget,
     window->priv->window_state = event->new_window_state;
     g_settings_set_int (window->priv->window_settings, XED_SETTINGS_WINDOW_STATE, window->priv->window_state);
 
-    return FALSE;
+    return GTK_WIDGET_CLASS (xed_window_parent_class)->window_state_event (widget, event);
 }
 
 static gboolean
